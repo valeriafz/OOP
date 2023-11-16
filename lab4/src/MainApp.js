9d46607896933709380c878140c65e09ae43f087
valeriafz <valeria.frunza33@gmail.com>
2023-11-16 18:49:14 +0200

    Fix peek 'Stack is empty' error

--- lab4/src/MainApp.js
+++ lab4/src/MainApp.js
@@ -9,23 +9,29 @@ var arrayStack = new ArrayStack_1.default();
 var linkedListStack = new LinkedListStack_1.default();
 var objectStack = new ObjectStack_1.default();
 
-const addElements = (...items) => {
-  arrayStack.push(...items);
-  linkedListStack.push(...items);
-  objectStack.push(...items);
+const addElements = function () {
+  for (let i = 0; i < arguments.length; i++) {
+    const item = arguments[i];
+    arrayStack.push(item);
+    linkedListStack.push(item);
+    objectStack.push(item);
+  }
+
   console.log("This is your array:");
-  console.log("Array Stack:", arrayStack.toArray());
-  console.log("Linked List Stack:", linkedListStack.toArray());
-  console.log("Object Stack:", objectStack.toArray());
+  console.log("Array Stack:", arrayStack);
+  console.log("Linked List Stack:", linkedListStack);
+  console.log("Object Stack:", objectStack);
 };
 
+addElements(1, 2, 3, 4);
+
+console.log("Array Stack Before Pop:", arrayStack);
 arrayStack.pop();
+console.log("Array Stack After Pop:", arrayStack);
 console.log("Array Stack Peek:", arrayStack.peek());
 
-linkedListStack.pop();
 console.log("Linked List Stack Peek:", linkedListStack.peek());
+linkedListStack.pop();
 
-objectStack.pop();
 console.log("Object Stack Peek:", objectStack.peek());
-
-addElements(1, 2, 3, 4);
+objectStack.pop();
--- lab4/src/MainApp.ts
+++ lab4/src/MainApp.ts
@@ -2,7 +2,6 @@ import ArrayStack from "./ArrayStack";
 import LinkedListStack from "./LinkedListStack";
 import ObjectStack from "./ObjectStack";
 
-// Initialize stacks
 const arrayStack = new ArrayStack<number>();
 const linkedListStack = new LinkedListStack<number>();
 const objectStack = new ObjectStack<number>();
@@ -14,18 +13,18 @@ const addElements = (...items: number[]) => {
     objectStack.push(item);
   }
   console.log("This is your array:");
-  console.log("Array Stack:", arrayStack.toArray());
-  console.log("Linked List Stack:", linkedListStack.toArray());
-  console.log("Object Stack:", objectStack.toArray());
+  console.log("Array Stack:", arrayStack);
+  console.log("Linked List Stack:", linkedListStack);
+  console.log("Object Stack:", objectStack);
 };
+addElements(1, 2, 3, 4);
 
+console.log("Array Stack Before Pop:", arrayStack);
 arrayStack.pop();
-console.log("Array Stack Peek:", arrayStack.peek());
+console.log("Array Stack After Pop:", arrayStack);
 
 linkedListStack.pop();
 console.log("Linked List Stack Peek:", linkedListStack.peek());
 
 objectStack.pop();
 console.log("Object Stack Peek:", objectStack.peek());
-
-addElements(1, 2, 3, 4);
